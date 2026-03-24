1c9b1463932b05c7fbf5290752fb8b72ff74ff83
hp-zed <krzydeix@outlook.com>
2026-03-24 23:23:20 +0800

    两节点连线测试

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+import NodesDemo from '@/view/practice/NodesDemo.vue'
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-
+      <NodesDemo/>
     </div>
   </header>
 </template>
---
+++ src/view/practice/NodesDemo.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { Graph } from '@antv/g6'
+
+const initData = {
+  nodes: [
+    {
+      id: 'node1',
+      x: 100,
+      y: 200,
+      label: '起始点',
+    },
+    {
+      id: 'node2',
+      x: 300,
+      y: 200,
+      label: '目标点',
+    },
+  ],
+  edges: [
+    {
+      source: 'node1',
+      target: 'node2',
+      label: '连线',
+    },
+  ],
+}
+
+onMounted(() => {
+  const graph = new Graph({
+    container: 'mountNode',
+    width: 800,
+    height: 600,
+  })
+  graph.data(initData);
+  graph.render()
+})
+</script>
+
+<template>
+  <div id="mountNode"></div>
+</template>
+
+<style scoped></style>
